86fd292ad008e04f26ef2610a22cdaf48c801b95
Neto <netod90@gmail.com>
2022-11-02 19:50:59 -0700

    Add toDoList structure

--- src/index.js
+++ src/index.js
@@ -12,3 +12,42 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+//START
+const toDoList = [
+  {
+    description: 'A list test 1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'A list test 2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'A list test 3',
+    completed: false,
+    index: 2,
+  }
+]
+
+const listContainer = document.querySelector('.item-list');
+
+toDoList.forEach((element) => {
+  const li = document.createElement('li');
+  li.classList.add('todo');
+  
+  let addElements = ``;
+
+  addElements += `
+    <input type="checkbox">
+    <div class="saved-item">
+      <label class="label">${element.description}</label>
+      <textarea class="edit" maxlength="255"></textarea>
+    </div>
+    <div class="item-icon"></div>
+  `
+  li.innerHTML = addElements;
+  listContainer.appendChild(li);
+})
