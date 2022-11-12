8b48674567e9515d772871d2ba5eeef0fa81f563
Neto <netod90@gmail.com>
2022-11-11 21:40:22 -0700

    Updated production-build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // development, production
+  mode: 'production', // development, production
   entry: './src/index.js',
   devServer: {
     static: './dist',
